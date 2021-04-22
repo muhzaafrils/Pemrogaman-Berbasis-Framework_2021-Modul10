d665f7ffeb8147ddc9268370e5c8f75393ca4e2e
muhzaafril <creeddarkkingdoms@gmail.com>
2021-04-23 00:06:55 +0700

    Modul 10

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default{
+  apiKey: "AIzaSyC_rF7gMwrYZ0cB3j6Lm4Ho_q-TGWr0k-0",
+  authDomain: "frameworkm10-baf19.firebaseapp.com",
+  projectId: "frameworkm10-baf19",
+  storageBucket: "frameworkm10-baf19.appspot.com",
+  messagingSenderId: "293450602596",
+  appId: "1:293450602596:web:c926f74b017a175a3f6771",
+  measurementId: "G-KT00BQXB2P"
+  };
